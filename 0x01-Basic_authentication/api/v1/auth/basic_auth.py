c70f6c57348aaf6117fa47082d9200851bbcf5f3
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-14 01:04:57 +0000

    decode auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,34 @@
 from typing import TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
+import base64
 
 
 class BasicAuth(Auth):
     """Basic auth class"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+            Returns auth header
+        """
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header.startswith('Basic '):
+            return authorization_header[6:]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """REturn a decoded base 64 auth value"""
+
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            code = base64.b64decode(
+                base64_authorization_header
+            )
+            return code.decode('utf-8')
+        except Exception:
+            return None
+
