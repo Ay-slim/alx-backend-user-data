01daa86ec8f7a3b81dab7b696df5d0a0e5aba5c1
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-14 01:07:42 +0000

    Current user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,3 +44,40 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+            Construct user instance from uname and pw
+        """
+        if type(user_email) is not str or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Fetches user instance from a request"""
+
+        head = self.authorization_header(request)
+        if head is None:
+            return None
+        extract = self.extract_base64_authorization_header(head)
+        if extract is None:
+            return None
+        decoded = self.decode_base64_authorization_header(extract)
+        if decoded is None:
+            return None
+        credentials = self.extract_user_credentials(decoded)
+        if credentials is None:
+            return None
+        user = self.user_object_from_credentials(
+            credentials[0],
+            credentials[1]
+        )
+        return user
