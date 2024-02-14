51f461cbcf61164d309fee1cc8938aff7920aa4c
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-14 01:05:08 +0000

    Pull us and pw

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,13 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """pulls email and password from encoded string"""
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return (email, password)
