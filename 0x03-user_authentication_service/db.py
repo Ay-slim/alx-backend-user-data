19a0b09708de92952aa01f2ebbab5848e239e0b8
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-20 22:31:00 +0000

    Try except

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,7 +36,11 @@ class DB:
         @hashed_password: User hashed password
         Return: Retunrs the new user object
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
