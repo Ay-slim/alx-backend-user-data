da5fdbc7845c7135a08dd2ad6c9648c48a4b3afe
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-21 13:48:07 +0000

    Password reset endpoints

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,5 +77,31 @@ def profile() -> str:
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """Endpoint for generating a password reset token"""
+
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({'email': email, 'reset_token': token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """Endpoint to change password and invalidate reset token"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    reset_token = request.form.get('new_password')
+
+    try:
+        AUTH.update_password(reset_token, password)
+        return jsonify({'email': email, 'message': 'Password updated'})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
