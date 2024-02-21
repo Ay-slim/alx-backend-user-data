5b4038dc17b2b49f297671488941728a4f071a4a
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-21 12:35:35 +0000

    Register user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,8 +18,21 @@ AUTH = Auth()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
-    """ the base index route for api """
+    """Base api route"""
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """Register a new user endpoint"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
