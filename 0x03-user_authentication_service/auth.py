e70c7f85394872a4b12322d2c7f164e070526c18
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-21 12:24:36 +0000

    Auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,3 +13,21 @@ def _hash_password(password: str) -> bytes:
     """
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """
+    Auth class to handle user authentication
+    """
+    def __init__(self):
+        """Initialize auth class"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """Registers a new user and validates password"""
+        try:
+            new_user = self._db.find_user_by(email=email)
+            return new_user
+        except Exception:
+            hashedpw = _hash_password(password)
+            return self._db.add_user(email, hashedpw)
